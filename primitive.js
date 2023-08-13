7a1bb2b237b0f63ee1756fba211fc8f2a4f789f9
kdrbipul <kdrbipul@gmail.com>
2023-08-13 10:06:53 +0600

    first commit

--- primitive.js
+++ primitive.js
@@ -12,3 +12,19 @@
 
 // Non Primitive
 // Object
+
+// let a ="Hello";
+// let b=a;
+// console.log(a,b);
+// b="Gello";
+// console.log(a,b);
+
+let x={
+    job:"web developer"
+}
+let y=x;
+console.log(x,y);
+y.job="Android Developer";
+console.log(x,y);
+
+// What is the difference between primitive and non primitive value
