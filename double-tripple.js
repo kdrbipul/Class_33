88441a24302b41a02554c7b4b6e275e50538db68
kdrbipul <kdrbipul@gmail.com>
2023-08-14 15:59:18 +0600

    first commit

---
+++ double-tripple.js
@@ -0,0 +1,14 @@
+const first = 3;
+const second = 3;
+
+if(first == second){
+    console.log("Condition is True");
+}else{
+    console.log("Condition is False");
+}
+
+
+const a = {name: "Abdul Kader"};
+const b = {name: "Abdul Kader"};
+
+console.log(JSON.stringify(a) === JSON.stringify(b));
--- true-false.js
+++ true-false.js
@@ -1,2 +1,8 @@
 "use strict";
 
+let x = NaN ;
+if (x){
+    console.log(x,"This is Truthy");
+}{
+    console.log(x, "This is falsy");
+}
