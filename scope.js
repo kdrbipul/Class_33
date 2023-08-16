0270e56e6603507e8f1cfe13461ad0faec0d642c
kdrbipul <kdrbipul@gmail.com>
2023-08-16 10:03:51 +0600

    first commit

---
+++ scope.js
@@ -0,0 +1,23 @@
+// const favNum = 70;
+// function add (first, second){
+//     const result =first +second;
+//     console.log(favNum);
+//     return result;
+// }
+
+// const sum = add(10,20);
+// console.log(sum);
+
+
+
+function add(first, second){
+    const result = first+second;
+    if(result > 9){
+        let mood = 'Happy';
+        console.log(mood);
+    }
+    return result;
+}
+
+const sum = add(10,20);
+console.log(sum);
---
+++ undefined-null.js
@@ -0,0 +1,7 @@
+const fifth ={name:"kader", age:23, location:darogar mor};
+console.log(fifth.phone);
+
+
+
+const six ={34,56,44,90};
+console.log(six[5]);
